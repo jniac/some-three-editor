119ecbb3f57484d9501e6ef76a1507e9a91b70a6
jniac <joseph.merdrignac@gmail.com>
2024-09-23 14:08:13 +0200

    (fix) ok, so "Object.defineProperty()" is readonly by default

--- src/editor-ui/three-display-names.ts
+++ src/editor-ui/three-display-names.ts
@@ -4,8 +4,14 @@ import { Group, InstancedMesh, Mesh, Object3D, Points, Scene } from 'three'
  * Preserves the names of Three.js classes through the `displayName` property.
  */
 export function initThreeDisplayNames() {
+  // Skip if not in the browser.
+  if (typeof window === 'undefined') {
+    return
+  }
+
   function set(target: object, displayName: string) {
-    Object.defineProperty(target, 'displayName', { value: displayName })
+    // Assigning static "displayName" property to the class.
+    Object.defineProperty(target, 'displayName', { value: displayName, writable: true })
   }
 
   set(Group, 'Group')
